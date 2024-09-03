75f7235d7bfa641255b969c36e832e8ee3b4f188
azra <91065112+muhammadazra4503@users.noreply.github.com>
2024-09-03 15:46:10 +0700

    Commit Main Menu + Stage Selection

---
+++ Assets/Script/UI/StageUIManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class StageUIManager : MonoBehaviour
+{
+    public Button[] levelButtons;
+    public Image[] lockedImages;
+
+    void Start()
+    {
+        for (int i = 1; i < levelButtons.Length; i++)
+        {
+            if (PlayerPrefs.GetInt("Level" + i + "Completed", 0) == 1)
+            {
+                levelButtons[i].interactable = true;
+                lockedImages[i].enabled = false;
+            }
+            else
+            {
+                levelButtons[i].interactable = false;
+                lockedImages[i].enabled = true;
+            }
+        }
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene("Level" + levelIndex);
+    }
+}
+
+
